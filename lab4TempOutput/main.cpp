d5a02ea26e66aacd0df213ababf964083dea7ff0
Jayce Abens <jayce_abens@s.webster-city.k12.ia.us>
2021-10-05 10:59:00 -0500

    documentation added

--- lab4TempOutput/main.cpp
+++ lab4TempOutput/main.cpp
@@ -30,12 +30,18 @@ int main()
 	return 0;
 }
 
+// Precondition : The iostream library is included. Using the namespace std
+// Postcondition : The contents of the header string are printed to the console and a new line is started ready for chart data to be printed
 void printHeading()
 {
 	const string heading = " Temp | -30  -15  0    15   30   45   60   75   90   105  120";
 	cout << heading << endl;
 }
 
+// Precondition : an input file is present at the file location listed below, containing numberical values separated by line or spaces. The iostream, iomanip, fstream, cmath, 
+//						and string libraries are included. Using the namespace std.
+// Postcondition : a line is prited to the screen for each temperature value containing the temperature value, and a bar displaying the value 1 star per 3 degrees. If a temperature 
+//						falls outside the expected range, the program should handle it and continue to the following values. The fuction should end when all values have bee printed
 void printChart()
 {
 	const string inputFileName = "C:/Users/Jayce Abens/OneDrive/Documents/C++2021/lab4TempOutput/lab4TempOutput/hourlyTemperatures.dat"; //input file location, temperature values are separated by line
@@ -44,15 +50,21 @@ void printChart()
 
 	float temp;
 	string bar;
-	while (!inFile.eof())	// repeats until the end of the file is reached (eof() returns nonzero value)
+	while (inFile)	// repeats until the end of the file is reached/stream is in a failed state
 	{
 		inFile >> temp;	// reads and stores the next temperature value
-		cout << setw(5) << setprecision(4) << temp << " |";	// prints the temperature with a constant width for alignment 
-		for (float f = abs(temp); f > 0; f = f - 3) bar.append("*"); // loops subracting 3 from the absolute value of the temperature, appends a * each time ( * = 3 degrees)
-		if (temp < 0) cout << setw(11) << bar << "|" << endl;	// if the temperature is negative, prints the bar left of zero with a set width for alignment
-		else cout << "           |" << bar << endl;	// if the temperature is positive, prints a space for negative values, then the positive bar
+		if (temp >= -30 && temp <= 120) // checks if temp value is within the expected range
+		{
+			cout << setw(5) << setprecision(4) << temp << " |";	// prints the temperature with a constant width for alignment 
+			for (float f = abs(temp); f >= 1.5; f = f - 3) bar.append("*"); // loops subracting 3 from the absolute value of the temperature, appends a * each time ( * = 3 degrees)
+			if (temp < 0) cout << setw(11) << bar << "|" << endl;	// if the temperature is negative, prints the bar left of zero with a set width for alignment
+			else cout << "           |" << bar << endl;	// if the temperature is positive, prints a space for negative values, then the positive bar
+		}
+		else
+		{
+			cout << "--- TEMPERATURE VALUE OUT OF RANGE ---" << endl;
+		}
 		bar = "";	// resets bar to empty
 	}
-
 	inFile.close(); // closes file stream
 }
